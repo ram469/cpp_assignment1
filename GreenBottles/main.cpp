afab63c6a5eb60c5055d177c4ba0c0bde7d3bad9
ramm <ram.physics69@gmail.com>
2019-01-10 22:22:41 +0530

    second assignment

---
+++ GreenBottles/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int greeBottles = 10;
+    cout<<"There were "<<greeBottles << " green bottle"<<endl;
+    greeBottles--;
+    cout<<"There were "<<greeBottles << " green bottle"<<endl;
+    return 0;
+}
